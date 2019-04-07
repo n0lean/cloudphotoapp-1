45e8d051d6733b523c072d0a382a7093201312f1
n0lean <cpy19960205@zju.edu.cn>
2019-04-07 16:56:52 -0400

    init commit

---
+++ src/main/java/com/clou/photoshare/PhotoshareApplication.java
@@ -0,0 +1,11 @@
+package com.clou.photoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoshareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoshareApplication.class, args);
+    }
+}
---
+++ src/test/java/com/clou/photoshare/PhotoshareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clou.photoshare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoshareApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
