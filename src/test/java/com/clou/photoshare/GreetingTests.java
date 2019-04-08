d787829e20ce4e2f0c1dda5a9a9d60f1c8012dc5
n0lean <cpy19960205@zju.edu.cn>
2019-04-07 22:16:27 -0400

    Some test unit test

--- src/test/java/com/clou/photoshare/GreetingTests.java
+++ src/test/java/com/clou/photoshare/GreetingTests.java
@@ -1,4 +1,17 @@
 package com.clou.photoshare;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GreetingTests {
+    @Test
+    public void greetingTest() {
+        Greeting obj = new Greeting(0, "1");
+    }
+
 }
