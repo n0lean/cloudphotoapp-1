a4f4e845b092763cd45dd1f7c37493ca3f8ac989
Anda <dennisan9665@gmail.com>
2019-04-17 11:16:20 -0400

    add exception controll

--- src/main/java/com/clou/photoshare/controller/UserController.java
+++ src/main/java/com/clou/photoshare/controller/UserController.java
@@ -2,10 +2,14 @@ package com.clou.photoshare.controller;
 
 import com.clou.photoshare.model.UserBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.clou.photoshare.model.User;
 import com.clou.photoshare.repository.UserRepository;
+
+import java.util.Optional;
 
 
 @RestController
@@ -21,14 +25,24 @@ public class UserController {
 
 
     @RequestMapping(value = "/save", method = RequestMethod.POST) // didn't use @ModelAttribute, decide later after know Congito
-    public String addUser(@RequestBody User user) {
-        repository.save(user);
-        return "success";
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        try{
+            repository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.toString());
+        }
+
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public User getUser(@PathVariable String id) {
-        return repository.findById(id).get(); // naive approach, return's a User JSON
+    public ResponseEntity<?> getUser(@PathVariable String id) {
+        // return repository.findById(id).get(); // naive approach, return's a User JSON
+
+        return Optional
+                .ofNullable(repository.findById(id))
+                .map(user -> ResponseEntity.ok().body(user))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.POST)
