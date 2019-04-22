97c1eaccc9281a5833a4d2c42f17e1913e5cdce1
Anda <dennisan9665@gmail.com>
2019-04-22 11:32:37 -0400

    add user search function

--- src/main/java/com/clou/photoshare/model/S3Address.java
+++ src/main/java/com/clou/photoshare/model/S3Address.java
@@ -1,10 +1,12 @@
 package com.clou.photoshare.model;
 
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
 import org.springframework.stereotype.Component;
 
 
 // function signiture only, @Hu Xin
+@Component
 public class S3Address {
     private String addressKey;
     private String addressBucket;
@@ -26,4 +28,39 @@ public class S3Address {
     public void setAddressBucket(String addressBucket) {
         this.addressBucket = addressBucket;
     }
+
+    static public class S3AddressConverter implements DynamoDBTypeConverter<String, S3Address> {
+
+        @Override
+        public String convert(S3Address s3address) {
+            String addressStr = null;
+            S3Address itemS3Address = (S3Address) s3address;
+
+            try {
+                if (itemS3Address != null) {
+                    addressStr = itemS3Address.getAddressBucket() + "@" + itemS3Address.getAddressKey();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return addressStr;
+        }
+
+        @Override
+        public S3Address unconvert(String s) {
+            S3Address s3Address = new S3Address();
+            try {
+                if (s != null && s.length() != 0) {
+                    String[] data = s.split("@");
+                    s3Address.setAddressBucket(data[0].trim());
+                    s3Address.setAddressKey(data[1].trim());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return s3Address;
+        }
+
+    }
+
 }
